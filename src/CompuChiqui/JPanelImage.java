fae56b5ed5f7f8665c095ac232e696e7c916d679
AUXPLANTA <AUXPLANTA@DESKTOP-U3EE586>
2016-04-22 17:02:35 -0500

    Clase que extiende de JPanel en donde se repinta la imagen a mostrar.

--- src/CompuChiqui/JPanelImage.java
+++ src/CompuChiqui/JPanelImage.java
@@ -4,23 +4,81 @@ package CompuChiqui;
  *
  * @author AUXPLANTA
  */
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
  
-public class JPanelImage extends JPanel {
+public class JPanelImage extends JPanel{
  
-    private Image imagen;
+    private Image imagen = new ImageIcon(getClass().getResource("/Imagenes/fondo.png")).getImage();
  
     public JPanelImage() {
+        
+        setDropTarget(new DropTarget(this, new DropTargetListener() {
+            @Override
+            public void dragEnter(DropTargetDragEvent dtde) {
+                setBorder(new LineBorder(new Color(221, 75, 57, 200), 2));
+            }
+
+            @Override
+            public void dragOver(DropTargetDragEvent dtde) {
+                
+            }
+
+            @Override
+            public void dropActionChanged(DropTargetDragEvent dtde) {
+                
+            }
+
+            @Override
+            public void dragExit(DropTargetEvent dte) {
+                setBorder(javax.swing.BorderFactory.createEtchedBorder());
+            }
+
+            @Override
+            public void drop(DropTargetDropEvent dtde) {
+                try {
+                    if (dtde.getDropAction() == 2) {
+                        dtde.acceptDrop(dtde.getDropAction());
+                        if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                            dtde.acceptDrop(DnDConstants.ACTION_COPY);
+                            List<File> lista = (List<File>)dtde.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
+                            if(lista.size()==1){                              
+                                setImagen(ImageIO.read(new File(lista.get(0).getAbsolutePath())));
+                                setBorder(javax.swing.BorderFactory.createEtchedBorder());
+                            }else if(lista.size()>1){
+                                JOptionPane.showMessageDialog(null, "SÓLO UNA IMGAEN A LA VEZ.");
+                            }
+                        }else{
+                            JOptionPane.showMessageDialog(null, "ÉSTE TIPO DE ARCHIVO NO ES SOPORTADO.");
+                        }
+                    }
+                } catch (Exception e) {
+                    
+                }
+            }
+        }));
+        
     }
  
     public JPanelImage(String nombreImagen){
         if (nombreImagen != null) {
-            imagen = new ImageIcon(
-                           getClass().getResource(nombreImagen)
-                           ).getImage();
+            imagen = new ImageIcon(getClass().getResource(nombreImagen)).getImage();
         }
     }
  
@@ -32,33 +90,38 @@ public class JPanelImage extends JPanel {
  
     public void setImagen(String nombreImagen) {
         if (nombreImagen != null) {
-            imagen = new ImageIcon(
-                   getClass().getResource(nombreImagen)
-                   ).getImage();
-        } else {
-            imagen = null;
+            imagen = new ImageIcon(getClass().getResource(nombreImagen)).getImage();
         }
- 
         repaint();
     }
  
     public void setImagen(Image nuevaImagen) {
         imagen = nuevaImagen;
- 
         repaint();
+    }
+    
+    public void setImagen(BufferedImage nuevaImagen){
+        imagen = nuevaImagen;
+        repaint();
+    }
+    
+    public BufferedImage getImagen(){
+        return (BufferedImage) imagen;
+    }
+    
+    public void setImagenDefault(){
+        imagen = new ImageIcon(getClass().getResource("/Imagenes/fondo.png")).getImage();
     }
  
     @Override
-    public void paint(Graphics g) {
+    public void paint(Graphics g){
         if (imagen != null) {
-            g.drawImage(imagen, 0, 0, getWidth(), getHeight(),
-                              this);
- 
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
             setOpaque(false);
         } else {
             setOpaque(true);
         }
- 
         super.paint(g);
     }
+
 }

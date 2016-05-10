d4c71f4715e284520be2271d2edc28dda0646f46
AUXPLANTA <AUXPLANTA@DESKTOP-U3EE586>
2016-05-10 07:50:24 -0500

    Añadida opcion para dibujar rectangulo.

--- src/CompuChiqui/JPanelImage.java
+++ src/CompuChiqui/JPanelImage.java
@@ -14,6 +14,9 @@ import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.List;
@@ -23,9 +26,12 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
  
-public class JPanelImage extends JPanel{
+public class JPanelImage extends JPanel implements MouseListener, MouseMotionListener{
  
     private Image imagen;// = new ImageIcon(getClass().getResource("/Imagenes/fondo.png")).getImage();
+    private boolean dibujar = false;
+    private int x1,y1,x2,y2,w,h;
+    private int x,y;
  
     public JPanelImage() {
         
@@ -109,13 +115,68 @@ public class JPanelImage extends JPanel{
  
     @Override
     public void paint(Graphics g){
+        super.paint(g);
         if (imagen != null) {
             g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
             setOpaque(false);
         } else {
             setOpaque(true);
         }
-        super.paint(g);
+        if(dibujar){
+            int width = this.x1 - this.x2;
+            int height = this.y1 - this.y2;
+            w = Math.abs( width );
+            h = Math.abs( height );
+            x = width < 0 ? x1 : x2;            
+            y = height < 0 ? y1 : y2;
+            g.drawRect( x, y , w, h );
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        dibujar = true;
+        x1 = e.getX();
+        y1 = e.getY();  
+        x2=x1;
+        y2=y1;
+        repaint();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        dibujar = false;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        x2 = e.getX();
+        y2 = e.getY();     
+        x = e.getX();
+        y = e.getY();       
+        repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();        
+        repaint();
     }
 
 }
